21da8bacf4f0c6ccb4bba21bb9c56bcc9fc02f82
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-05-04 13:58:45 +0200

    adding required functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function App() {
-  return <div className="App">Quiz</div>;
-}
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>REACT-TS QUIZ</h1>
+      <button className="start" onClick={startTrivia}></button>
+    </div>
+  );
+};
 
 export default App;
