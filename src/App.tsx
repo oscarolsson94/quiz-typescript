95853c7d6b0d67a76731eb043863f01e244cf04b
oscarolsson94 <oscar.olsson94@hotmail.com>
2021-05-05 09:05:31 +0200

    implementing checkAnswer functionality

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,26 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      // Users answer
+      const answer = e.currentTarget.value;
+      // Check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // Add score if answer is correct
+      if (correct) {
+        setScore((prev) => prev + 1);
+      }
+      // Save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
 
   const nextQuestion = () => {};
 
